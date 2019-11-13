c9837c8ead8bd47fbe63c1f9dd8afcf1f56b439a
book81able <ecobb81@gmail.com>
2019-11-13 09:34:12 -0500

    Some Changes

--- SpyAccelroationPrototype/Assets/Scripts/Prototype.cs
+++ SpyAccelroationPrototype/Assets/Scripts/Prototype.cs
@@ -16,9 +16,6 @@ public class Prototype : MonoBehaviour
     public float wait = 4f;
     public GameObject capturePointBadge;
 
-    public Color caughtColor;
-    public Color SafeColor;
-
     private bool hasBeenCaught = false;
 
     public void capturePlayer(GameObject player)
@@ -35,31 +32,25 @@ public class Prototype : MonoBehaviour
     /// </summary>
     public void captureTower(GameObject tower)
     {
-        tower.SetActive(true);
         capturePointBadge.SetActive(true);
-        StartCoroutine(deactivate(tower, wait));
 
     }
 
     public IEnumerator deactivate(GameObject obj,float waitTime)
     {
         yield return new WaitForSeconds(waitTime);
-        obj.SetActive(false);
     }
 
+    // Lock Button press
     public void CaptureSystem(Button captureButton)
     {
-        Text captureButtonText = captureButton.GetComponentInChildren(typeof(Text)) as Text;
         if (hasBeenCaught)
         {
-            captureButtonText.text = "I've been caught";
-            captureButton.GetComponent<Image>().color = caughtColor;
+
             hasBeenCaught = false;
         }
         else
         {
-            captureButtonText.text = "I'm at base";
-            captureButton.GetComponent<Image>().color = SafeColor;
             capturePointBadge.SetActive(false);
             hasBeenCaught = true;
         }
